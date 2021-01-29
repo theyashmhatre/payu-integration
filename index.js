0bbdb68e303dad0029300ada2c5c88f787300306
YashMhatre <yashmhatre62@gmail.com>
2021-01-29 13:45:02 +0530

    added save to db functionality

--- index.js
+++ index.js
@@ -4,42 +4,23 @@ const PORT = process.env.PORT || 4000;
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-var jsSHA = require("jssha");
 var crypto = require('crypto');
 const request = require('request');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const path = require('path');
-const {nanoid} = require("nanoid");
-
-
-// returns the calculated hash value
-app.post("/api/get-hash",(req,res) => {
-    console.log("get hash executed");
-    console.log(req.body);
-    var pd = req.body;
-
-    var hashString = pd.key + '|' + pd.txnid + '|' + pd.amount + '|' + pd.productinfo + '|'+ pd.firstname + '|' + pd.email + '|' + '||||||||||' + pd.salt; // Your salt value
-    var sha = new jsSHA('SHA-512', "TEXT");
-    sha.update(hashString);
-    var hash = sha.getHash("HEX");
-    res.status(200).json({ 'hash': hash });
-    console.log(hash);
-});
+const db = require("./db");
+const ResponseInfo = require("./models/txnDetails");
 
 
 //sends a post request to the test payumoney url. 
 app.post("/api/payumoney", urlencodedParser, (req,res) => {
-    console.log(" payu response Executed");
-
 
     try {
-        if (!req.body.amount || !req.body.firstname || !req.body.email)
+        if (!req.body.amount || !req.body.firstname || !req.body.email || !req.body.childId || !req.body.commission)
             {res.status(400).json({ msg: "Mandatory fields missing" }).end();
             throw new Error('Mandatory fields missing');
         }
-
-        console.log(req.body);
 
         let key = 'BC50nb';
         let salt = 'Bwxo1cPe';
@@ -84,6 +65,7 @@ app.post("/api/payumoney", urlencodedParser, (req,res) => {
 
         console.log(data);
 
+        //sending a post request to the Payumoney URL with the data
         request.post({
             headers: {
                 'Content-Type': 'application/json',
@@ -107,7 +89,6 @@ app.post("/api/payumoney", urlencodedParser, (req,res) => {
                 httpRes.statusCode <= 400) {
                 res.redirect(httpRes.headers.location.toString());  // redirects to the payment url after 302 code
                 console.log(httpRes.statusCode, httpRes.headers.location.toString());
-                console.log("error 300 and 400");
             }
         });
     } catch (err) {
@@ -127,28 +108,66 @@ app.post('/payment/success', urlencodedParser, (req, res) => {
     let amount = req.body.amount;
     let productinfo = req.body.productinfo;
     let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
+    let phone = req.body.phone;
     let email = req.body.email;
     let status = req.body.status;
+    let additionalCharges = req.body.additionalCharges;
+    let cardnum = req.body.cardnum;
+    let payuMoneyId = req.body.payuMoneyId;
+    let mihpayid = req.body.mihpayid;
+    let mode = req.body.mode;
+    let PG_TYPE = req.body.PG_TYPE;
+    let bank_ref_num = req.body.bank_ref_num;
 
-    var hashString = salt + '|' + status + '|' + '||||||||||' + email + '|' + firstname + '|' + productinfo + '|' + amount + '|' + txnid + '|' + key;
-
-    console.log(hashString);
+    //calculates the hash
+    var hashString = additionalCharges + '|' + salt + '|' + status + '||||||||||' + '|' + email + '|' + firstname + '|' + productinfo + '|' + amount + '|' + txnid + '|' + key;
     var cryp = crypto.createHash('sha512');
     cryp.update(hashString);
     var hash = cryp.digest('hex');
 
 
-    //checking if the two hashes are equal. and then completing the txn. else, cancel it.
-    if (req.body.hash === hash) { console.log("hash values matched!");  }
-    else { console.log("values didnt match!!!", req.body.hash, hash); }
+    //checking if the two hashes are equal. and then completing the txn. else, txn is not valid and someone's rigging it.
+    if (req.body.hash === hash) 
+        { 
+            console.log("hash values matched!");  
+            const newResponse = new ResponseInfo({
+                mode,
+                key,
+                status,
+                amount,
+                productinfo,
+                firstname,
+                email,
+                PG_TYPE,
+                bank_ref_num,
+                payuMoneyId,
+                mihpayid,
+                cardnum,
+                lastname,
+                phone,
+                additionalCharges,
+            });
+
+            newResponse.save()
+            .then((result)=> {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    else { 
+        console.log("values didnt match!!!", req.body.hash, hash); 
+    }
 
     res.send(req.body);
 });
 
 
 
-
-app.post('/api/verifyHash', urlencodedParser, (req,res) => {
+//for testing purpose only.  /api/success does the job
+app.post('/api/verifyHash', urlencodedParser, async (req,res) => {
     console.log("success executed", req.body);
 
     let key = 'BC50nb';
@@ -157,27 +176,59 @@ app.post('/api/verifyHash', urlencodedParser, (req,res) => {
     let amount = req.body.amount;
     let productinfo = req.body.productinfo;
     let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
+    let phone = req.body.phone;
     let email = req.body.email;
     let status = req.body.status;
+    let additionalCharges = req.body.additionalCharges;
+    let cardnum = req.body.cardnum;
+    let payuMoneyId = req.body.payuMoneyId;
+    let mihpayid = req.body.mihpayid;
+    let mode = req.body.mode;
+    let PG_TYPE = req.body.PG_TYPE;
+    let bank_ref_num = req.body.bank_ref_num;
 
-    var hashString = salt + '|' + status + '|' + '||||||||||' + email + '|' + firstname + '|' + productinfo + '|' + amount + '|' + txnid + '|' + key;
+    var hashString = additionalCharges + '|' + salt + '|' + status + '||||||||||' + '|' + email + '|' + firstname + '|' + productinfo + '|' + amount + '|' + txnid + '|' + key;
 
     console.log(hashString);
     var cryp = crypto.createHash('sha512');
     cryp.update(hashString);
     var hash = cryp.digest('hex');
 
-    if (req.body.hash === hash) { console.log("hash values matched!"); res.send("hurray!!!"); }
+    if (req.body.hash === hash) {
+        console.log("hash values matched!");
+        const newTxn = new ResponseInfo({
+            mode,
+            key,
+            status,
+            amount,
+            productinfo,
+            firstname,
+            email,
+            PG_TYPE,
+            bank_ref_num,
+            payuMoneyId,
+            mihpayid,
+            cardnum,
+            lastname,
+            phone,
+            additionalCharges,
+        });
+
+        const savedTxn = await newTxn.save();
+        console.log(savedTxn);
+        res.status(200).json(savedTxn); 
+    }
     else { console.log("values didnt match!!!", req.body.hash, hash); res.send("bad luck!"); }
 });
 
 
 app.post('/payment/fail', urlencodedParser , (req, res) => {
     //Payumoney will send Fail Transaction data to req body. 
-    // Based on the response Implement UI as per you want
+    // Based on the response Implement UI as you want
     res.send(req.body);
     console.log("failure executed", req.body);
-})
+});
 
 
 app.use(express.static(path.join(__dirname, './build')));
